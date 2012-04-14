23879662b392ffa456e421605188d26c46e23545
Julio Cesar de Souza <julio@julioc.me>
2012-04-14 02:43:26 -0300

    Implemented MeshData methods

--- mesh.cpp
+++ mesh.cpp
@@ -2,31 +2,129 @@
 
 #include <string.h>
 
-MeshData::Attribute::Attribute(uint vertices,
+MeshData::Attribute::Attribute(uint nVertices,
                                const char* identifier,
                                DataType dataType,
                                size_t dataSize,
-                               uint components = 4) :
+                               uint nComponents) :
   _identifier(NULL),
   _dataType(dataType),
   _dataSize(dataSize),
-  _components(components),
-  _size(dataSize * components),
+  _nComponents(nComponents),
+  _size(dataSize * nComponents),
   _data(NULL)
 {
   _identifier = new char[strlen(identifier) + 1];
   strcpy(identifier, _identifier);
 
   // Allocate size for the components of every vertex
-  _data = new char[_size * vertices];
+  _data = new char[_size * nVertices];
 }
 
-MeshData::Attribute::~Attribute() {
-  delete _identifier;
-  delete _data;
+MeshData::Attribute::~Attribute()
+{
+  delete[] _identifier;
+  delete[] _data;
 }
 
-void MeshData::Attribute::set(uint vertexId, const void *data) {
+void MeshData::Attribute::set(uint vertexId, const void *data)
+{
   memcpy(&(_data[vertexId * _size]), data, _size);
 }
 
+MeshData::MeshData(const char *identifier, uint nVertices, uint nTriangles) :
+  _identifier(NULL),
+  _nVertices(nVertices),
+  _nTriangles(nTriangles),
+  _vPosition(NULL),
+  _vNormal(NULL),
+  _vAttributes(NULL),
+  _triangles(NULL)
+{
+  _identifier = new char[strlen(identifier) + 1];
+  strcpy(identifier, _identifier);
+
+  _vPosition = new float[nVertices * 4];
+  _vNormal = new float[nVertices * 4];
+
+  for(int i = 0; i < MESH_MAX_ATTRIBUTES; ++i) {
+    _vAttributes[i] = NULL;
+  }
+
+  _triangles = new uint[nTriangles * 3];
+}
+
+MeshData::~MeshData()
+{
+  for(int i = 0; i < MESH_MAX_ATTRIBUTES; ++i) {
+    if(_vAttributes[i] != NULL) {
+      delete _vAttributes[i];
+    }
+  }
+
+  delete[] _identifier;
+  delete[] _vPosition;
+  delete[] _vNormal;
+  delete[] _triangles;
+}
+
+int MeshData::regAttribute(const char* identifier,
+                           DataType dataType,
+                           size_t dataSize,
+                           uint nComponents)
+{
+  int index;
+  for(index = 0; index < MESH_MAX_ATTRIBUTES; ++index) {
+    if(_vAttributes[index] == NULL) {
+      break;
+    }
+  }
+
+  if(index == MESH_MAX_ATTRIBUTES) {
+    return -1;
+  }
+
+  _vAttributes[index] = new Attribute(_vertices, identifier, dataType, dataSize, nComponents);
+  return index;
+}
+
+bool MeshData::setPosition(uint vertexId, float x, float y, float z, float w)
+{
+  if(vertexId >= _nVertices) {
+    return false;
+  }
+
+  int offset = vertexId * 4;
+  _vPosition[offset + 0] = x;
+  _vPosition[offset + 1] = y;
+  _vPosition[offset + 2] = z;
+  _vPosition[offset + 3] = w;
+
+  return true;
+}
+
+bool MeshData::setNormal(uint vertexId, float x, float y, float z, float w)
+{
+  if(vertexId >= _nVertices) {
+    return false;
+  }
+
+  int offset = vertexId * 4;
+  _vNormal[offset + 0] = x;
+  _vNormal[offset + 1] = y;
+  _vNormal[offset + 2] = z;
+  _vNormal[offset + 3] = w;
+
+  return true;
+}
+
+bool MeshData::setAttribute(uint vertexId, uint attributeId, const void *data)
+{
+  if(vertexId >= _nVertices || _vAttributes[attributeId] == NULL) {
+    return false;
+  }
+
+  _vAttributes[attributeId]->set(vertexId, data);
+
+  return true;
+}
--- mesh.h
+++ mesh.h
@@ -2,6 +2,9 @@
 #define MESH_H
 
 #include <stdlib.h>
+#include <GL/gl.h>
+
+#define MESH_MAX_ATTRIBUTES 16
 
 class MeshData
 {
@@ -21,19 +24,19 @@ protected:
   class Attribute
   {
   public:
-    Attribute(uint vertices,
+    Attribute(uint nVertices,
               const char *identifier,
               DataType dataType,
               size_t dataSize,
-              uint components = 4);
+              uint nComponents = 4);
     ~Attribute();
 
     inline const char* identifier() const { return _identifier; }
     inline DataType dataType() const { return _dataType; }
     inline size_t dataSize() const { return _dataSize; }
-    inline uint components() const { return _components; }
+    inline uint nComponents() const { return _nComponents; }
 
-    inline const void* data() const { return _data; };
+    inline const void* data() const { return _data; }
 
     void set(uint vertexId, const void *data);
 
@@ -41,7 +44,7 @@ protected:
     const char *_identifier;
     DataType _dataType;
     size_t _dataSize;
-    uint _components;
+    uint _nComponents;
 
     size_t _size;
 
@@ -49,26 +52,30 @@ protected:
   };
 
 public:
-  MeshData(const char* identifier,
-           uint vertices,
-           uint triangles);
+  MeshData(const char* identifier, uint nVertices, uint nTriangles);
   ~MeshData();
 
   const char* identifier() const { return _identifier; }
 
-  uint registerAttribute(const char* identifier,
-                         DataType dataType,
-                         size_t dataSize,
-                         uint components = 4);
+  int regAttribute(const char* identifier,
+                   DataType dataType,
+                   size_t dataSize,
+                   uint nComponents = 4);
 
   bool setPosition(uint vertexId, float x, float y, float z, float w = 1.0);
   bool setNormal(uint vertexId, float x, float y, float z, float w = 0.0);
-  bool setAttribute(uint vertexId, uint attribute, const void* data);
+  bool setAttribute(uint vertexId, uint attributeId, const void* data);
 
 protected:
-  float **_vPosition;
-  float **_vNormal;
-  Attribute *_vAttributes;
+  const char *_identifier;
+  uint _nVertices;
+  uint _nTriangles;
+
+  float *_vPosition;
+  float *_vNormal;
+  Attribute *_vAttributes[MESH_MAX_ATTRIBUTES];
+
+  uint *_triangles;
 };
 
 #endif // MESH_H
