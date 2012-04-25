3caef18bb6f76a0c41ee039b9b6eb48f5ebf3081
Julio Cesar de Souza <julio@julioc.me>
2012-04-25 02:24:13 -0300

    Added simple shading on height scene

--- shaders/height/fshader.glsl
+++ shaders/height/fshader.glsl
@@ -1,16 +1,18 @@
 #version 330 core
 
+in vec4 v_Position;
+in vec4 v_Normal;
 in float v_Height;
 
 out vec4 FragColor;
 
 void main() {
-  FragColor.g = 0.8*v_Height;
-  FragColor.b = 0.1*v_Height;
-  FragColor.r = 0.6;
+  vec3 color = vec3(0.8*v_Height, 0.1*v_Height, 0.6);
 
+  vec3 lightdir = -v_Position.xyz;
+
+  float i = max(dot(normalize(lightdir), normalize(v_Normal.xyz)), 0.0);
+
+  FragColor.xyz = color * i;
   FragColor.a = 1.0;
 }
-
-
-
--- shaders/height/vshader.glsl
+++ shaders/height/vshader.glsl
@@ -1,11 +1,19 @@
-#version 330 core
+#version 330
 
-attribute vec3 a_Position;
+in vec3 a_Position;
+in vec3 a_Normal;
 
+out vec4 v_Position;
+out vec4 v_Normal;
 out float v_Height;
 
+uniform mat4 ModelViewMatrix;
 uniform mat4 ModelViewProjectionMatrix;
+uniform mat3 NormalMatrix;
+
 void main() {
-  gl_Position = ModelViewProjectionMatrix * vec4(a_Position, 1.0);
+  gl_Position = ModelViewProjectionMatrix * vec4(a_Position.xyz, 1.0);
+  v_Position = ModelViewMatrix * vec4(a_Position.xyz, 1.0);
+  v_Normal = vec4(NormalMatrix * a_Normal, 0.0);
   v_Height = 0.5 + (a_Position.y * 2);
 }
